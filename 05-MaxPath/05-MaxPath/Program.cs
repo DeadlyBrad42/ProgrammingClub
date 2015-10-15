c2069e03f7f1cfccf4333fa248397c71afc6752a
Brad Mason <deadlybrad42@gmail.com>
2015-10-15 01:21:25 -0400

    Initial challenge commit

---
+++ 05-MaxPath/05-MaxPath/Program.cs
@@ -0,0 +1,71 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_MaxPath
+{
+	class Program
+	{
+		public const int TimesToRun = 3;
+		public const string FilePath = @"input1.txt";
+		public const bool LoggingEnabled = true;
+
+		static void Main(string[] args)
+		{
+			List<Stopwatch> _runTimers = new List<Stopwatch>();
+
+			for (int _runCount = 0; _runCount < TimesToRun; _runCount++)
+			{
+				// Read file into memory
+				string[] _lines = File.ReadAllLines(FilePath);
+
+				// Initialize & start stopwatch
+				Stopwatch _stopwatch = new Stopwatch();
+				_stopwatch.Start();
+
+				// Print run information
+				if (LoggingEnabled)
+				{
+					Console.WriteLine("Run #" + (_runCount + 1));
+				}
+
+				// stuff here
+
+
+				// Stop the timer and store it
+				_stopwatch.Stop();
+				_runTimers.Add(_stopwatch);
+
+				// Print run information
+				if (LoggingEnabled)
+				{
+					Console.WriteLine("Run " + (_runCount + 1) + " elapsed time: " + FormatTimeSpan(_stopwatch.Elapsed));
+				}
+			}
+
+			// Average run time
+			Console.WriteLine();
+			TimeSpan _averageRunTime = TimeSpan.FromMilliseconds(_runTimers.Average(x => x.Elapsed.TotalMilliseconds));
+			Console.WriteLine("Average run time: " + FormatTimeSpan(_averageRunTime));
+			Console.WriteLine();
+
+			// Bye
+			System.Console.ReadKey();
+		}
+
+		private static string FormatTimeSpan(TimeSpan timespan)
+		{
+			return String.Format(
+				"{0:00}h {1:00}m {2:00}s {3:00}ms",
+				timespan.Hours,
+				timespan.Minutes,
+				timespan.Seconds,
+				timespan.Milliseconds
+			);
+		}
+	}
+}
---
+++ 05-MaxPath/MaxPath.md
@@ -0,0 +1,18 @@
+# MaxPath
+
+We are going to break from tradition and do a challenge from something other one from "Programming Challenges: The Programming Contest Training Manual".  Next month's challenge is brought to you by the great people at [Project Euler](https://projecteuler.net).  
+
+From: https://projecteuler.net/problem=18 & https://projecteuler.net/problem=67
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+```
+              3
+             7 4
+            2 4 6
+           8 5 9 3
+```
+That is, 3 + 7 + 4 + 9 = 23.
+
+
+
+Attached are two files to test your code against.  p018_triangle.txt is one that you could technically complete the task using brute force calculations, but p067_triangle.txt is one that will not allow you to do that.  We want to solve the larger file for our challenge.
